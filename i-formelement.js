e77fa31362ae18061ba69db597f273b081cf7c22
Marco Asbreuk <marco@itsasbreuk.nl>
2015-05-25 21:30:07 +0200

    intermediate saving

--- i-formelement.js
+++ i-formelement.js
@@ -10,9 +10,33 @@ module.exports = function (window) {
     if (!window.ITAGS[itagName]) {
 
         Itag = DOCUMENT.defineItag(itagName, {
+            attrs: {
+                'i-prop': 'string'
+            },
+
             init: function() {
                 this.setAttr('itag-formelement', 'true', true);
+            },
+
+            valueProperty: 'value', // this is the default, but could be overridden
+
+            getValue: function() {
+                var element = this;
+                return element.model[element.valueProperty];
+            },
+
+            currentToReset: function() {
+                var element = this,
+                    model = element.model;
+                model['reset-value'] = model[element.valueProperty];
+            },
+
+            reset: function() {
+                var element = this,
+                    model = element.model;
+                model[element.valueProperty] = model['reset-value'];
             }
+
         });
 
         window.ITAGS[itagName] = Itag;
